9d60d9c99ad940b6ef87d26479c909852c1c066c
joshuacoyne <josh.j.coyne@gmail.com>
2018-09-26 13:29:28 -0400

    started server and model

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+const config = require('./config');
+
+mongoose.connect('mongodb://localhost:27017/forget-me-knot', {useNewUrlParser: true})
+        .then(() => {
+            console.log('Connected to DB');
+        })
+        .catch(() => {
+            console.log('Failed to connect to DB');
+        });
+
+const server = express();
+
+// use middleware
+server.use(express.json());
+server.use(helmet());
+
+server.use(
+    session({
+      secret: config.sessionSecret,
+      cookie: { maxAge: 1 * 24 * 60 * 60 * 1000 },
+      secure: false,
+      httpOnly: true,
+      name: "quilly-sessions",
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({
+        mongooseConnection: mongoose.connection,
+        ttl: 1000 * 60 * 60 * 24
+      })
+    })
+  );
+
+  server.get('/', (req, res) => {
+      res.json({ 'Forget me knot api': 'running'})
+  });
+
+  server.listen(config.port, () => {
+      console.log(`API running on port ${config.port}`);
+  });
+
